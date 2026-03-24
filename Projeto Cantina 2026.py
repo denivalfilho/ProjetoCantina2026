f63431955e7de7b7d237f15efb1cadd467826d3f
Denival Biotto Filho <denivaldenival@gmail.com>
2026-03-24 09:28:21 -0300

    Update Projeto Cantina 2026.py

--- Projeto Cantina 2026.py
+++ Projeto Cantina 2026.py
@@ -38,10 +38,11 @@ class Estoque:
         return None
 
 class Pagamento:
-    def __init__(self, pagador, categoria, curso, valor, data_hora):
+    def __init__(self, pagador, categoria, curso, item, valor, data_hora):
         self.pagador = pagador
         self.categoria = categoria
         self.curso = curso
+        self.item = item
         self.valor = valor
         self.data_hora = data_hora
         self.proximo = None
@@ -50,32 +51,41 @@ class HistoricoVendas:
     def __init__(self):
         self.inicio = None
 
-    def registrar_pagamento(self, novo_pagamento):
+    def adicionar_pagamento(self, novo):
         if self.inicio is None:
-            self.inicio = novo_pagamento
+            self.inicio = novo
         else:
             atual = self.inicio
             while atual.proximo is not None:
                 atual = atual.proximo
-            atual.proximo = novo_pagamento
+            atual.proximo = novo
 
-    def gerar_extrato(self, filtro_categoria=None):
+    def gerar_extrato(self, filtro=None):
         atual = self.inicio
         print("--- EXTRATO DE PAGAMENTOS ---")
         while atual is not None:
-            if filtro_categoria is None or atual.categoria == filtro_categoria:
-                print(f"Pagador: {atual.pagador} | Cat: {atual.categoria} | Curso: {atual.curso} | Valor: R$ {atual.valor}")
+            if filtro is None or atual.categoria == filtro:
+                print("Pagador:", atual.pagador, "| Item:", atual.item, "| Valor: R$", atual.valor)
             atual = atual.proximo
 
+    def relatorio_consumo(self):
+        atual = self.inicio
+        print("--- RELATORIO DE CONSUMO ---")
+        while atual is not None:
+            print("O(a)", atual.pagador, "consumiu:", atual.item, "em:", atual.data_hora)
+            atual = atual.proximo
+
+# --- TESTES ---
 cantina = Estoque()
 historico = HistoricoVendas()
 
-item1 = ItemEstoque("Coxinha", 3.50, 6.00, "15/03", "18/03", 10)
-cantina.adicionar(item1)
+it1 = ItemEstoque("Coxinha", 3.50, 6.00, "15/03", "18/03", 10)
+cantina.adicionar(it1)
 
+# Ao vender, pegamos o objeto e passamos os dados para o historico
 venda = cantina.vender("Coxinha")
 if venda:
-    p1 = Pagamento("Maria", "Professor", "ESG", venda.preco_venda, "22/03 15:30")
-    historico.registrar_pagamento(p1)
+    p1 = Pagamento("Maria", "Professor", "ESG", venda.nome, venda.preco_venda, "22/03 15:30")
+    historico.adicionar_pagamento(p1)
 
-historico.gerar_extrato()
+historico.relatorio_consumo()
